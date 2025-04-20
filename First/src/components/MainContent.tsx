b0711c215e0f454d0d3aedddbd399e9f7558c663
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-21 03:45:35 +0530

    First Main

---
+++ First/src/components/MainContent.tsx
@@ -0,0 +1,9 @@
+export const MainContent = () => {
+    return(
+        <main>
+            <h2>Main content</h2>
+            <p>Finally First is completed</p>
+        </main>
+            
+    )
+}
