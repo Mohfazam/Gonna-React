8e447d8f7a600e8d53b5c78b6033279dfbfaa29e
Mohfazam <mohd.sarwar.code@gmail.com>
2025-04-23 18:22:20 +0530

    Map FUnction and metho

--- First/src/components/List.tsx
+++ First/src/components/List.tsx
@@ -1,6 +1,13 @@
 export const List = () => {
 
     const numbers = [1,2,3,4,5];
+    const fruits = ["apple", "banana", "cherry", "mango"];
+    const users = [
+        { name: "Alice", age: 20 },
+        { name: "Bob", age: 25 },
+        { name: "Charlie", age: 30 }
+      ]
+      
 
     return (
         <div className="border-red-700 border-4 m-4 p-4">
@@ -10,6 +17,23 @@ export const List = () => {
                         Number: {number}
                     </li>
             ))}
+            </ul>
+            <ul>
+                {fruits.map((fruit) => (
+                    <li>
+                        Fruit Name: {fruit}
+                    </li>
+                ))}
+            </ul>
+            <br />
+            <p className="border-b-4">Users:</p>
+            <br />
+            <ul>
+                {users.map((user) => (
+                    <li>
+                        {user.name} is {user.age} old
+                    </li>
+                ))}
             </ul>
         </div>
     )
