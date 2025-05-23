08dced068b7f898264269e9a2d671b4efb7b81ec
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-24 02:25:31 +0530

    Weather

--- second/src/App.tsx
+++ second/src/App.tsx
@@ -35,9 +35,8 @@ function App() {
 
   return (
     <>
-    <button className="p-4 m-4 border-blue-800 border-6 bg-red-400 hover:bg-cyan-800" onClick={() => setFlag(!flag)}> Click me</button>
      <div className="p-2 m-2 border-amber-950 border-4 w-fit ">
-      <Weather temprature={20} />
+      <Weather temprature={27} />
      </div>
     </>
   )
--- second/src/components/Weather.tsx
+++ second/src/components/Weather.tsx
@@ -2,10 +2,14 @@ interface weatherProps {
     temprature: number;
 }
 
-export const Weather  = ({temprature}:weatherProps) => {
-return (
-    <div>
-        {temprature > 25? <p>hot Outside</p> :  <p>cold</p>}
-    </div>
-)
+export const Weather = ({ temprature }: weatherProps) => {
+    return (
+        <div>
+            {
+                temprature > 25 ? <p>hot Outside</p> :
+                temprature > 10 && temprature < 25? <p>Normla Outside</p> :
+                <p>Cold Outside</p> 
+            }
+        </div>
+    )
 }
