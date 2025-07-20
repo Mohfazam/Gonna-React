a380bcf6103d83572803fe340eb5727db83ced48
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-20 22:42:13 +0530

    Medium 1

--- HOOKS/useState()/MEDIUM-1/src/App.tsx
+++ HOOKS/useState()/MEDIUM-1/src/App.tsx
@@ -2,23 +2,33 @@ import { useState } from 'react'
 
 import './App.css'
 
-// interface TaskType{
-//   task: string;
-//   status: boolean;
-// }
+interface TaskType{
+  task: string;
+  status: string;
+}
 
 function App() {
   const [inputValue, setinputValue] = useState("");
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState<TaskType[]>([]);
 
   const handleTask = () => {
+
+    if(inputValue.trim() === '') return;
+
     const newTask = {
       task: inputValue,
       status: "due"
     }
 
-    setTask()
+    setTask([...task, newTask]);
+    setinputValue("")
   };
+
+  const handleDelete = (index:number) => {
+      const updatedtask = task.filter((_, i) => i !== index);
+
+      setTask(updatedtask);
+  }
 
   return (
     <>
@@ -28,11 +38,27 @@ function App() {
 
       <div>
         <strong>Task:</strong>
-        <input type="text" placeholder='Enter Task' onChange={(e) => setinputValue(e.target.value)}/>
+        <input type="text" placeholder={inputValue === '' ? "Enter task" : inputValue} onChange={(e) => setinputValue(e.target.value)}/>
       </div>
 
       <div>
-        <button onClick={setTask(() => )}>Add task</button>
+        <button onClick={handleTask}>Add task</button>
+      </div>
+
+      <div>
+        <h2>Tasks:</h2>
+        {task.length === 0 ? (
+          <p>No tasks yet</p>
+        ) : (
+          <ul>{task.map((item, index) => (
+            <li key={index}>
+              <strong>{item.task}</strong>
+              &nbsp;&nbsp;
+              <em>{item.status}</em>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </li>
+          ))}</ul>
+        )}
       </div>
     </>
   )
