3d53de52ca5af4948b9eea2dfd7f09402685035e
Mohfazam <mohd.sarwar.code@gmail.com>
2025-09-25 02:31:10 +0530

    query

--- tanstack-Query/src/component/PostList.tsx
+++ tanstack-Query/src/component/PostList.tsx
@@ -1,7 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../api/api";
+
 export const PostList = () => {
-    return(
-        <>
-        Hello Nigga
-        </>
-    )
-}
+  const { data, isLoading, isError } = useQuery<{ id: string; title: string }[]>({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+    staleTime: 10000,
+    refetchOnWindowFocus: true,
+  });
+
+  if (isError) return <div>Error fetching posts</div>;
+
+  return (
+    <>
+      {isLoading ? (
+        <div>Loading data...</div>
+      ) : (
+        data?.map((post) => <div key={post.id}>{post.title}</div>)
+      )}
+    </>
+  );
+};
