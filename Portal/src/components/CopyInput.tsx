57470f7d3c7d4f6510be6c604fc402f96672738c
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-04 13:09:43 +0530

    Clipboard copy

--- Portal/src/components/CopyInput.tsx
+++ Portal/src/components/CopyInput.tsx
@@ -2,10 +2,21 @@ import { useState } from "react"
 
 export const CopyInput = () => {
 
-    const [input, setInput] = useState("");
+    const [inputValue, setInput] = useState("");
     const [copied, setCopied] = useState(false);
 
-    return(
-        <div>Copy Input</div>
+    const handleCopy = () => {
+        navigator.clipboard.writeText(inputValue).then(() =>{
+            setCopied(true);
+
+            setTimeout(() => setCopied(false), 2000);
+        })
+    }
+
+    return (
+        <div>
+            <input type="text" value={inputValue} onChange={(e) => { setInput(e.target.value) }} />
+            <button onClick={handleCopy}>Copy</button>
+        </div>
     )
 }
