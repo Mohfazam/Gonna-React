fc5a73d479c3e9f2de379e19767ae0bfb68f2f39
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-19 17:06:32 +0530

    Sudo Keyframer

--- Framer-Motion/src/Components/Keyframe.tsx
+++ Framer-Motion/src/Components/Keyframe.tsx
@@ -5,7 +5,9 @@ const Keyframe = () => {
       <motion.div className="box" 
       
       animate={{
-        scale: [1, 2, 3, 3, 2, 1]
+        // scale: [1, 2, 3, 3, 2, 1]
+        rotate: [0, 20, 30, 50, 60, 80, 120, 190, 220, 250, 250,220, 180, 120, 80, 50, 20, 0],
+        borderRadius:['20%', '25%',  '35%', '45%', '60%', '70%', '80%', '90%', '100%']
       }}
       transition={{
         delay: 2,
